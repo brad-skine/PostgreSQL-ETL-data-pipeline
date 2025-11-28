1476877e178d1f199e1264a0494bcf6d67cc50a1
brad-skine <138541961+brad-skine@users.noreply.github.com>
2025-11-28 15:01:43 +1300

    etl pipeline fixes giving expected results

--- scripts/load_warehouse.sql
+++ scripts/load_warehouse.sql
@@ -1,10 +1,20 @@
+-- FUNCTION: etl.load_warehouse()
+
+-- DROP FUNCTION IF EXISTS etl.load_warehouse();
+
+CREATE OR REPLACE FUNCTION etl.load_warehouse(
+	)
+    RETURNS void
+    LANGUAGE 'sql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
 INSERT INTO warehouse.dim_customer (name, city, zip)
 SELECT DISTINCT 
 	customer_name as name,
 	NULL AS city,
 	NULL::INT AS zip
 FROM staging.sales_cleaned;
-
 
 INSERT INTO warehouse.dim_date (order_date, year, month, day)
 SELECT DISTINCT 
@@ -15,14 +25,11 @@ SELECT DISTINCT
 FROM staging.sales_cleaned
 WHERE order_date IS NOT NULL;
 
-
-INSERT INTO warehouse.dim_product (name, price, catagory)
+INSERT INTO warehouse.dim_product (name, catagory)
 SELECT DISTINCT 
 	product_name AS name,
-	price,
 	NULL as catagory
 FROM staging.sales_cleaned;
-
 
 INSERT INTO warehouse.dim_promotion (name,ad_type,coupon_type,
 	discount,start_date, end_date, active, updatetime)
@@ -37,8 +44,6 @@ SELECT DISTINCT  -- default values
     CURRENT_TIMESTAMP AS updatetime
 FROM staging.sales_cleaned
 WHERE promotion_name IS NOT NULL;
-
-
 
 INSERT INTO warehouse.fact_sales (customer_id, product_id, 
 promotion_id, date_id, units_sold, revenue)
@@ -59,7 +64,8 @@ JOIN warehouse.dim_product pr ON s.product_name = pr.name
 LEFT JOIN warehouse.dim_promotion pro ON s.promotion_name = pro.name
 JOIN warehouse.dim_date d ON s.order_date = d.order_date;
 
-	
-	
 
-	
+$BODY$;
+
+ALTER FUNCTION etl.load_warehouse()
+    OWNER TO postgres;
--- sql/test.sql
+++ sql/test.sql
@@ -1,6 +1,10 @@
--- SELECT etl.delete_sales_tables();
--- SELECT etl.clear_warehouse();
--- SELECT * FROM etl.etl_sales_pipeline();
+SELECT etl.delete_sales_tables();
+SELECT etl.clear_warehouse();
+SELECT * FROM etl.etl_sales_pipeline();
 
 
-SELECT * FROM warehouse.fact_sales;
+
+SELECT * FROM staging.sales_cleaned;
+
+SELECT * FROM warehouse.dim_product;
+-- SELECT * FROM warehouse.fact_sales;
